47b9c1dbd5f0e26f072bd33a4158a470fdb867f4
Marian Ovidiu Hutanu <h.marian914@gmail.com>
2021-01-26 17:44:55 +0100

    aggiunto bonus

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 var app = new Vue({
   el : '#root',
   data : {
-    sciao: 'Sciao belo, kebab con o senza scipola?'
-  }
+    sciao: 'Sciao belo, kebab con o senza scipola?',
+    altezza : '',
+    lunghezza: '',
+    coloreSfondo: '',
+    coloreScritta: '',
+    bordi: '',
+    arrodondamento: '',
+    testo: '',
+    contenitore: 'box'
+  },
+  methods: {
+    siONo : function() {
+      alert('Non mi interessa ti meto scipola lo stesso')
+    }
   }
 })
